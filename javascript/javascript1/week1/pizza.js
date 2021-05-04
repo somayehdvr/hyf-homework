3bcd228612ab2c950a87a66480e440d2e59c966c
somayehdvr <somayehdvr@gmail.com>
2021-05-05 00:23:49 +0200

    pizza exercise

---
+++ javascript/javascript1/week1/pizza.js
@@ -0,0 +1,28 @@
+console.log("I love pizza")
+
+let name = 'benjamin';
+name = 'benjamin-better';
+
+const pizzaPrice = 78;
+const pizzaPriceDiscounted = pizzaPrice - 10;
+
+const users = ['peter', 'Johnny', 'Børge'];
+const lastUser = users[2];
+
+/////////////////////////
+
+let favPizza
+favPizza = 'pepperony'
+let price
+price = 100
+console.log('New pizza order:', favPizza,'. The price of the pizza is: ',price)
+
+/////////////////////////
+
+let amountOfPizza = 2
+let familySize = 1
+let totalPrice = 100
+if (familySize) {
+    totalPrice = totalPrice * 2 
+}
+console.log('New pizza order:',amountOfPizza,familySize,favPizza,'. Total cost for the order is:',totalPrice)
