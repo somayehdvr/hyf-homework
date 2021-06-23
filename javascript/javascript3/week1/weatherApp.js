d96f31c916bdc1da1b5caac4a7ad1b6390800eb7
somayehdvr <somayehdvr@gmail.com>
2021-06-23 17:11:59 +0200

    javascript/javascript3/week1

---
+++ javascript/javascript3/week1/weatherApp.js
@@ -0,0 +1,19 @@
+document.querySelector(".submitCity").addEventListener("click", () => printWeather())
+
+function printWeather() {
+    const city = document.querySelector(".city").value
+
+    // Fetch weather json data
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=85bf3bafaf784a301c66129107680db7`)
+        .then(response => response.json())
+        .then(weatherData => {
+            console.log(weatherData);
+            const weather = document.querySelector(".weather")
+            weather.innerHTML = `city: ${city} , <br /> 
+            temperature: ${weatherData.main.temp} F ${weatherData.weather[0].description}, <br /> 
+            Icon for the weather type: <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png"> , <br /> 
+            Wind speed: ${weatherData.wind.speed} <br /> 
+            How clowdy it is: ${weatherData.clouds.all}% <br /> 
+            sunrise is: ${weatherData.sys.sunrise} and sunset is: ${weatherData.sys.sunset}`
+        });
+}
