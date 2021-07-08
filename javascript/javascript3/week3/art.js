ff70164cc472ae6c65449cda8ab87297031a3271
somayehdvr <somayehdvr@gmail.com>
2021-07-09 01:08:21 +0200

    update

--- javascript/javascript3/week3/art.js
+++ javascript/javascript3/week3/art.js
@@ -2,4 +2,22 @@ var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 ctx.beginPath();
 ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
+ctx.fillstyle = "red";
+ctx.fill();
+
+class Circle {
+    constructor(x, y, r, startAngle, endAngle, fillColor) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+        this.fillColor = fillColor;
+    }
+    draw(){
+        
+    }
+}
+
+const c1 = new Circle(50, 50, 20, 0, 2 * Math.PI, "#000000");
+c1.draw();
