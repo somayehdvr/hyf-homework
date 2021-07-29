20ff21734ffd0dfe071902c642ae30f87149c80c
somayehdvr <61237342+somayehdvr@users.noreply.github.com>
2021-07-29 20:45:25 +0200

    Update homework.js
    Co-authored-by: Joachim Koch <joachimvkoch@gmail.com>

--- javascript/javascript3/week2/homework.js
+++ javascript/javascript3/week2/homework.js
@@ -90,12 +90,10 @@ new Promise(resolve => {
 //using async/await
 async function fetchUsingAsync() {
     try {
-        const asyncResponse = await setTimeout(() => {
-            fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json')
-                .then(response => response.json())
-                .then(moviesData => {
-                    console.log(moviesData)
-                })
+        const asyncResponse = await setTimeout(async () => {
+            const response = await fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json')
+            const moviesData = await response.json()
+            console.log(moviesData)
         }, 3000);
         return asyncResponse;
     } catch(error) {
