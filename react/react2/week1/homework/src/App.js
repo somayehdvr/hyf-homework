2b9b38ed49d1a120d0f99333167612bbdd9f4886
somayehdvr <somayehdvr@gmail.com>
2022-05-18 00:12:23 +0200

    react-react2-week1

--- react/react2/week1/homework/src/App.js
+++ react/react2/week1/homework/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useCallback, useEffect, useState} from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState("")
+  const [user, setUser] = useState("SomayehDvr")
+  const [API, setAPI] = useState("https://api.github.com/search/users?q=SomayehDvr")
+  const inputHandler = (input) => {
+    setUser(input.target.value)
+    setAPI("https://api.github.com/search/users?q=" + user )
+  }
+  const fetchAPI = useCallback(() => {
+    return fetch(API)
+      .then(results => results.json())
+      .then(data => {
+        setData(data)
+        setLoading(false)
+      })
+      //  When the search is failed, you should show "error fetching (reason why it failed)..." 
+      .catch(error => console.error("error fetching " + error))
+  }, [API])
+  useEffect(() => {
+    fetchAPI()
+  }, [fetchAPI])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Github user searcher </h1>
+      {/* When the search is loading, show "loading..." */}
+      {loading ? "Loading" : 
+        <>
+          <input value={user} onChange={inputHandler} placeholder="Search for user" />
+          {/* When the search result is empty (no users), you should show "No results..." */}
+          {data.total_count === 0 ? "No results..." :
+            <>
+              {data.items.map(item => {
+                return item.login + item.url
+              })}
+            </>
+          }
+        </>
+      }
+      
+     
+      {/* All state should be managed using the context api! */}
     </div>
   );
 }
